41904ec8f883789b2b78d22ec7786b65cf17d5ef
Kareem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-06-24 14:39:01 +0300

    Add Types Of Products

---
+++ types/index.ts
@@ -0,0 +1,51 @@
+export interface Brand {
+  id: number;
+  name: string;
+}
+
+export interface BrandFilterProps {
+  onFilterChange: (brandIds: number[]) => void;
+}
+
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Props {
+  onSelect: (selectedIds: number[]) => void;
+}
+
+export interface SubCategory {
+  id: number;
+  name: string;
+  category_id: number;
+}
+
+export interface Props {
+  selectedCategoryIds: number[];
+  onSelect: (ids: number[]) => void;
+}
+
+export interface Product {
+  id: number;
+  name?: string;
+  name_en?: string;
+  price?: number | string;
+  image?: string;
+  rate_avg?: number | string;
+}
+
+export interface Filters {
+  rating: number | null;
+  price: string | null;
+  categoryIds: number[];
+  brandIds: number[];
+  subCategoryIds: number[];
+}
+
+export interface ProductListProps {
+  filters?: Filters;
+}
+
