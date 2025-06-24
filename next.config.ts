6c3249e5d5efec4618671e6f640152129b1a7ae7
Kareem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-06-24 14:40:20 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "backend.outletplus.sa",
+        pathname: "/storage/**",
+      },
+      {
+        protocol: "https",
+        hostname: "up.outletplus.sa",
+        pathname: "/products/**", 
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse3.mm.bing.net',
+        pathname: '/th/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
